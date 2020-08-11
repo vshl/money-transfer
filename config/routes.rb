f53854bc8aa2ec8f0708eb43c308e189897999a4
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-08-11 18:11:22 +0530

    Added User and Account controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/transfer' => 'transfer#send_money'
+  resources :users
+  resources :accounts
 end

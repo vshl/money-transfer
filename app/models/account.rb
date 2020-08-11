cc9f4c44d285938554b64710340b5bf84271c7b5
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-08-11 15:30:16 +0530

    Helper methods for the models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,16 @@
 class Account < ApplicationRecord
   belongs_to :user
   has_many :transactions
+
+  def savings?
+    type == 'Accounts::SavingsAccount'
+  end
+
+  def current?
+    type == 'Accounts::CurrentAccount'
+  end
+
+  def basic_savings?
+    type == 'Accounts::BasicSavingsAccount'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_many :accounts
+
+  def total_balance
+    accounts.sum(&:balance)
+  end
 end

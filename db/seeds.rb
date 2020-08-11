d04c66fca4f31f9d4cb3df4206dc001334b6df01
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-08-11 17:53:54 +0530

    Creating seed data for local database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+10.times do |_x|
+  User.create(name: Faker::Name.name)
+end
+
+20.times do |_x|
+  Account.create(
+    name: Faker::Bank.name,
+    type: ['Accounts::SavingsAccount',
+           'Accounts::CurrentAccount',
+           'Accounts::BasicSavingsAccount'].sample,
+    balance: Faker::Number.decimal(l_digits: 2),
+    user_id: (1..10).to_a.sample
+  )
+end

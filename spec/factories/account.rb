3366792066eacbd227b7b0688c56a2609c9e8d50
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-08-11 15:29:46 +0530

    Account factory

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
   factory :account do
     name { Faker::Bank.name }
     type { ['Accounts::SavingsAccount',
-            'Account::CurrentAccount',
-            'Account::BasicSavingsAccount'].sample }
+            'Accounts::CurrentAccount',
+            'Accounts::BasicSavingsAccount'].sample }
     balance {  Faker::Number.decimal(l_digits: 2) }
     user
   end
